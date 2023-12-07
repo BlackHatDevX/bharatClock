6ac4f4616908fc498f43cb5ef870d721fb810489
BlackHatDevX <jashgro@yandex.com>
2023-12-07 15:08:50 +0530

    +useEffect

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
+
 const Time = () => {
-  const d = new Date();
-  let currentTime = d.toLocaleDateString() + " " + d.toLocaleTimeString();
+  const [d, setD] = useState(new Date());
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setD(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, []);
+
   return (
     <>
-      <p className="lead ">This is the current time:{currentTime}</p>
+      <p className="lead ">
+        This is the current time:
+        {d.toLocaleDateString() + " " + d.toLocaleTimeString()}
+      </p>
     </>
   );
 };
